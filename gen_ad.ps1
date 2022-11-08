0e1aa33ccfbed796c985173414f814a1fe9ba70d
unknown <Adminstrator@xyz.com>
2022-11-08 14:59:47 +0000

    Updating the script with Undo Options

--- gen_ad.ps1
+++ gen_ad.ps1
@@ -1,4 +1,7 @@
-param( [Parameter(Mandatory=$true)] $JSONFile)
+param( 
+    [Parameter(Mandatory=$true)] $JSONFile,
+    [switch]$Undo
+    )
 
 function CreateADGroup {
     param ([Parameter(Mandatory=$true)] $groupObject)
@@ -45,24 +48,51 @@ function CreateADUser {
     }
 }
 
-function WeakenPasswordPolicy() {
-    secedit /export /cfg C:\windows\tasks\secpol.cfg
-    (Get-Content C:\Windows\Tasks\secpol.cfg).replace("PasswordComplexity = 1", "PasswordComplexity = 0") | Out-File C:\Windows\Tasks\secpol.cfg
-    secedit /configure /db C:\windows\security\local.sdb /cfg C:\Windows\Tasks\secpol.cfg /areas SECURITYPOLICY
-    rm -force C:\Windows\Tasks\secpol.cfg -confirm:$false
-    
+function RemoveADUser(){
+    param( [Parameter(Mandatory=$true)] $userObject )
+
+    $name = $userObject.name
+    $firstname, $lastname = $name.Split(" ")
+    $username = ($firstname[0] + $lastname).ToLower()
+    $samAccountName = $username
+    Remove-ADUser -Identity $samAccountName -Confirm:$False
 }
 
-WeakenPasswordPolicy
+function WeakenPasswordPolicy(){
+    secedit /export /cfg C:\Windows\Tasks\secpol.cfg
+    (Get-Content C:\Windows\Tasks\secpol.cfg).replace("PasswordComplexity = 1", "PasswordComplexity = 0").replace("MinimumPasswordLength = 7", "MinimumPasswordLength = 1") | Out-File C:\Windows\Tasks\secpol.cfg
+    secedit /configure /db c:\windows\security\local.sdb /cfg C:\Windows\Tasks\secpol.cfg /areas SECURITYPOLICY
+    rm -force C:\Windows\Tasks\secpol.cfg -confirm:$false
+}
 
-$json = ( Get-Content $JSONFile | ConvertFrom-Json )
+function StrengthenPasswordPolicy(){
+    secedit /export /cfg C:\Windows\Tasks\secpol.cfg
+    (Get-Content C:\Windows\Tasks\secpol.cfg).replace("PasswordComplexity = 0", "PasswordComplexity = 1").replace("MinimumPasswordLength = 1", "MinimumPasswordLength = 7") | Out-File C:\Windows\Tasks\secpol.cfg
+    secedit /configure /db c:\windows\security\local.sdb /cfg C:\Windows\Tasks\secpol.cfg /areas SECURITYPOLICY
+    rm -force C:\Windows\Tasks\secpol.cfg -confirm:$false
+}
 
+$json = ( Get-Content $JSONFile | ConvertFrom-JSON)
 $Global:Domain = $json.domain
 
-foreach ( $group in $json.groups ) {
-    CreateADGroup $group
-}
+if ( -not $Undo) {
+    WeakenPasswordPolicy
 
-foreach ( $user in $json.users ) {
-    CreateADUser $user
+    foreach ( $group in $json.groups ){
+        CreateADGroup $group
+    }
+    
+    foreach ( $user in $json.users ){
+        CreateADUser $user
+    }
+}else{
+    StrengthenPasswordPolicy
+
+    foreach ( $user in $json.users ){
+        RemoveADUser $user
+    }
+    foreach ( $group in $json.groups ){
+        RemoveADGroup $group
+    }
 }
+Footer
